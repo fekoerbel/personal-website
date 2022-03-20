bdae9024c8de1a3d6aaa3bf015b9e2c67bbcb4b6
fekoerbel <fekoerbel@hotmail.com>
2022-03-20 12:53:22 -0300

    logo link

--- src/components/templates/logo.jsx
+++ src/components/templates/logo.jsx
@@ -1,11 +1,12 @@
 import "./logo.css"
 import React from "react";
 import logo from "../../assets/logo-felipe1.png"
+import { NavLink } from 'react-router-dom';
 
 export default class Logo extends React.Component {
     render() {
         return (
-            <img src={logo} alt="Logo Felipe Koerbel" />
+            <NavLink to="/"><img src={logo} alt="Logo Felipe Koerbel" /></NavLink>
         )
     }
 } 

bb8dbb5de312bf8946ab413a534f0353b223abb2
fekoerbel <fekoerbel@hotmail.com>
2022-03-20 14:52:34 -0300

    offcanvas changes

--- src/components/templates/offcanvasMenu.jsx
+++ src/components/templates/offcanvasMenu.jsx
@@ -7,6 +7,14 @@ export default class MenuOffcanvas extends React.Component {
     render() {
         return (
             <Menu className='menu-offcanvas-display' right>
+                <header className='header-offcanvas'>
+                    <h1>
+                    FELIPE KOERBEL
+                    </h1>
+                    <h2>
+                    PERSONAL WEBSITE
+                    </h2>
+                </header>
                 <NavLink to="/" activeclassname="active" target={'_parent'}>Home</NavLink>
                 <NavLink to="/resume" target={'_parent'}>Resume</NavLink>
                 <NavLink to="/portfolio" target={'_parent'}>Portfolio</NavLink>

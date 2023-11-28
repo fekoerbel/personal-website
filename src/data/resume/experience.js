f5e1fa81f7ffc3bbf103098616debfd384c9a887
Felipe Koerbel dos Anjos <fekoerbel@hotmail.com>
2023-11-28 10:06:32 -0300

    Update experience.js

--- src/data/resume/experience.js
+++ src/data/resume/experience.js
@@ -1,9 +1,26 @@
 const experiences = [
+        {
+      company: 'Clickright Soluções em Tecnologia',
+      position: 'Front-end Developer',
+      link: 'https://www.clickright.com.br/',
+      daterange: 'April 2022 - Present',
+      points: [
+    'At Clickright, I develop dynamic pages, forms, functionalities, and reusable stylized components using HTML, CSS, SASS, Bootstrap, JavaScript, TypeScript, Angular, PHP, and Laravel, as well as CMS systems like WordPress and Joomla!. I also utilize Git for version control, with GitHub as my primary repository.',
+    'My contributions include:',
+    'Abastece-aí Futebol - Implemented various updates in Angular (7.x), including new validations in the checkout process, improvements in the list of purchased tickets, updates in ticket details, and the installation of Google Recaptcha. Applied general style changes throughout the system. The project was built using Angular with SCSS, and TypeScript was used as the main language.',
+    'Maracanã Stadium Website - Developed most of the static pages of the website and updated the homepage using PHP in a Laravel project with Bootstrap.',
+    'Flamengo Website - Developed a feature that allows the creation of an image gallery in the transparency section, including necessary front-end reflections. The project was created using Laravel with Bootstrap and PHP.',
+    'Pawradar - Complete front-end development of Pawradar, a pet adoption portal, including all pages and some features such as the ability to add banners. The project was created using Laravel with Bootstrap and PHP, as well as JavaScript (DOM) for certain tasks.',
+    'Champions Club (under development) - Complete front-end development, including all pages and various backend CRUDS. The project was created using Laravel with Bootstrap and PHP, as well as JavaScript (DOM) for certain tasks. Laravel Livewire was also used to make some components dynamic without page reloading.',
+    'Azure Imóveis (under development) - Complete front-end development, including all pages and various backend CRUDS. Developed an intelligent real estate search system. The project was created using Laravel with Bootstrap and PHP, as well as JavaScript (DOM) for certain tasks.',
+    'Financial Flow (internal system) - Fully developed the front-end of Financial Flow, an internal system that records all income and expense entries separated by months of the year, allowing the division of entries into subgroups. It also allows the printing of reports. In addition to front-end development, I worked as a junior developer on the back-end, assisting other developers in implementing features. The project was created using InertiaJs, a framework for creating SPAs without the need to create an API, with Laravel and React. The Ant Design framework was used for styling.'
+],
+    },
     {
       company: 'Clickright Soluções em Tecnologia',
       position: 'Front-end Developer(Intern)',
       link: 'https://www.clickright.com.br/',
-      daterange: 'October 2021 - Present',
+      daterange: 'October 2021 - April 2022',
       points: [
         'Developed dynamic landing pages, forms, functionalities and reusable styled components;', 'HTML;', 'CSS/SASS;', 'Bootstrap;', 'Javascript;', 'Typescript;', 'Angular;', 'PHP;', 'Laravel;', 'Joomla!;', 'Git;', "Trello."
       ],
